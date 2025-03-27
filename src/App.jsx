6c7dcdabe037cb63b62acc693a7068c471bf49b2
Zer0 <gruchaczovy@gmail.com>
2025-03-27 18:24:02 +0100

    finnish był

--- src/App.jsx
+++ src/App.jsx
@@ -30,35 +30,44 @@ let r4 = 2*x*y+15*x*y;
 let r5 = 3*x**2+15*y+2;
 
   return (
-    <>
-    <div class="rown1">
-        <p>Mój komputer ma numer {n}</p>
-        <p>{n}*3 + 2y - {n} = {v1}</p>
-        </div>
+      <>
+          <header>
+              <div class="podpis">Mój komputer ma numer {n}</div>
 
-    <div class="rown2">
-<p>
-{n} * 1 = {w1}<br></br>
-{n} * 2 = {w2}<br></br>
-{n} * 3 = {w3}<br></br>
-{n} * 4 = {w4}<br></br>
-{n} * 5 = {w5}<br></br>
-{n} * 6 = {w6}<br></br>
-{n} * 7 = {w7}<br></br>
-{n} * 8 = {w8}<br></br>
-{n} * 9 = {w9}<br></br>
-{n} * 10 = {w10}<br></br>
-</p>
-    </div>
+</header>
+    <div class="par">
+            <div class="rown ro2">
+                <p>
+                {n} * 1 = {w1}<br></br>
+                {n} * 2 = {w2}<br></br>
+                {n} * 3 = {w3}<br></br>
+                {n} * 4 = {w4}<br></br>
+                {n} * 5 = {w5}<br></br>
+                {n} * 6 = {w6}<br></br>
+                {n} * 7 = {w7}<br></br>
+                {n} * 8 = {w8}<br></br>
+                {n} * 9 = {w9}<br></br>
+                {n} * 10 = {w10}<br></br>
+                </p>
+          </div>
 
-    <div class="rown3">
-      <p>
-    15x<sup>3</sup>+1-15y = {r1}<br></br>
-    2x+315-4x<sup>2</sup> = {r2}<br></br>
-    3y<sup>2</sup>-15x<sup>2</sup>y-7y = {r3}<br></br>
-    2xy+15xy; = {r4}<br></br>
-    3x<sup>2</sup>+15y+2 = {r5}
-    </p>
+              <div class="rown ro1">
+                  <p>z = {z}</p><br></br>
+                <p>{n} * 3 + 2z - {n} = {v1}</p>
+          </div>
+
+
+              <div class="rown ro3">
+                  <p>x = {x} y = {y}</p><br></br>
+
+                  <p>
+                    15x<sup>3</sup>+1-15y = {r1}<br></br>
+                    2x+315-4x<sup>2</sup> = {r2}<br></br>
+                    3y<sup>2</sup>-15x<sup>2</sup>y-7y = {r3}<br></br>
+                    2xy+15xy; = {r4}<br></br>
+                    3x<sup>2</sup>+15y+2 = {r5}
+                  </p>
+              </div>
     </div>
   </>
 
